3247aff9871ed271bd5412607df638b6d869c05f
Max Schillinger <maxschillinger@web.de>
2023-05-15 21:15:25 +0200

    cffi_extension_build.py: Read C header instead duplicating it

--- cffi/cffi_extension_build.py
+++ cffi/cffi_extension_build.py
@@ -1,17 +1,10 @@
 from cffi import FFI
 ffibuilder = FFI()
 
-ffibuilder.cdef("""\
-typedef struct Bigram {
-    int letter1AsciiCode;
-    int letter2AsciiCode;
-    double frequency;
-} Bigram;
-double test_single_layout(char* layout, int layout_length, Bigram* bigrams,
-                          int bigrams_count, double* score_list);
-void get_top_scores(char** layouts, long layout_count, double* scores, int nr_of_best,
-                    int letters_per_layer, char** top_layouts, double* top_scores);
-""")
+with open('cffi_extension.h') as f:
+    header = f.read()
+
+ffibuilder.cdef(header)
 
 ffibuilder.set_source("_cffi_extension",  # name of the output C extension
 """
